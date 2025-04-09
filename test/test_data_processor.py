d1da9a2e99cbb67e8a709dc09a70cd95ad8aec4f
JMojicaR <jrodmoj@gmail.com>
2025-04-09 00:00:28 -0600

    Add modifications

--- test/test_data_processor.py
+++ test/test_data_processor.py
@@ -1,13 +1,11 @@
-import unittest
+import pytest
 from app.data_processor import process_data
-import os
 
-class TestDataProcessor(unittest.TestCase):
-
-    def test_process_data(self):
-        data = {'age': [23,29,21,28], 'height': [165,170,174,168]}
-        result = process_data(data)
-        self.assertEqual(result.loc['mean', 'age'], 25.25)
-
-    if __name__=='__main__':
-        reports_dir = os.path.join(os.getcwd(), 'reports')
+def test_process_data():
+    """
+    Test the process_data function to ensure it calculates the correct mean.
+    """
+    data = {'age': [23, 29, 21, 28], 'height': [165, 170, 174, 168]}
+    result = process_data(data)
+    assert result.loc['mean', 'age'] == 25.25
+    assert result.loc['mean', 'height'] == 169.25
