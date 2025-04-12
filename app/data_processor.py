3bf28c3c68ab7349860dda8505be9da613b25503
JMojicaR <jrodmoj@gmail.com>
2025-04-12 13:11:43 -0600

    add modification

--- app/data_processor.py
+++ app/data_processor.py
@@ -7,7 +7,7 @@ def process_data(data):
 def main():
     data = {'age': [23, 29, 21, 18], 'height':[165, 170, 174, 168]}
     result = process_data(data)
-    print(result)
+    print(f'Resultado: {result}')
 
 if __name__=='__main__':
     main()
