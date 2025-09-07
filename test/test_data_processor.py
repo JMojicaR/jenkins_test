247bbbf648ef3b7cf277ffa524a06997240fbb3a
JMojicaR <jrodmoj@gmail.com>
2025-09-07 17:12:36 -0600

    convert_test_into_class

--- test/test_data_processor.py
+++ test/test_data_processor.py
@@ -1,29 +1,30 @@
 import pytest
 from app.data_processor import process_data
 
-def test_process_data():
-    """
-    Test the process_data function to ensure it calculates the correct mean.
-    """
-    data = {'age': [23, 29, 21, 28], 'height': [165, 170, 174, 168]}
-    result = process_data(data)
-    assert result.loc['mean', 'age'] == 25.25
-    assert result.loc['mean', 'height'] == 169.25
-
-def test_process_data2():
-    """
-    Test the process_data function to ensure it calculates the correct mean.
-    """
-    data = {'age': [23, 29, 21, 28], 'height': [165, 170, 174, 168]}
-    result = process_data(data)
-    assert result.loc['mean', 'age'] == 24.25
-    assert result.loc['mean', 'height'] == 169.25
-
-def test_process_data3():
-    """
-    Test the process_data function to ensure it calculates the correct mean.
-    """
-    data = {'age': [23, 29, 21, 28], 'height': [165, 170, 174, 168]}
-    result = process_data(data)
-    assert result.loc['mean', 'age'] == 23.25
-    assert result.loc['mean', 'height'] == 169.25
+class TestDataProcessor:
+    def test_process_data():
+        """
+        Test the process_data function to ensure it calculates the correct mean.
+        """
+        data = {'age': [23, 29, 21, 28], 'height': [165, 170, 174, 168]}
+        result = process_data(data)
+        assert result.loc['mean', 'age'] == 25.25
+        assert result.loc['mean', 'height'] == 169.25
+    
+    def test_process_data2():
+        """
+        Test the process_data function to ensure it calculates the correct mean.
+        """
+        data = {'age': [23, 29, 21, 28], 'height': [165, 170, 174, 168]}
+        result = process_data(data)
+        assert result.loc['mean', 'age'] == 24.25
+        assert result.loc['mean', 'height'] == 169.25
+    
+    def test_process_data3():
+        """
+        Test the process_data function to ensure it calculates the correct mean.
+        """
+        data = {'age': [23, 29, 21, 28], 'height': [165, 170, 174, 168]}
+        result = process_data(data)
+        assert result.loc['mean', 'age'] == 23.25
+        assert result.loc['mean', 'height'] == 169.25
